1ac2fdd099379bcb26a5e53048006ea893df42fd
Stefano Ordine <stefano.ordine@gmail.com>
2016-09-09 12:19:25 +0200

    Calculate angles for all bone fingers.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const Leap = require('leapjs');
+const glMatrix = require('gl-matrix');
 const { Controller } = Leap;
 
 const controllerOptions = {
@@ -9,6 +10,30 @@ const controllerOptions = {
   useAllPlugins: false
 };
 
-var controller = new Controller(controllerOptions)
-  .connect()
-  .on('frame', frame => { console.log(frame); });
+const printFingersAngles = (matrix) => {
+  console.log('| finger | alpha | beta | gamma |');
+  matrix.forEach(({ type, alpha, beta, gamma }) => {
+    console.log(`| ${type} | ${alpha} | ${beta} | ${gamma} |`);
+  })
+}
+
+const FINGER_NAMES = ['THUMB', 'INDEX', 'MIDDLE', 'RING', 'PINKY'];
+
+const calculateAngle = (v1, v2) => (glMatrix.vec3.angle(v1, v2) * 180 / Math.PI).toPrecision(2);
+
+const sampleAngles = () => {
+  const { fingers } = controller.frame();
+  const samples = fingers
+    .map(({ metacarpal, proximal, medial, distal, type }) => {
+      const alpha = calculateAngle(metacarpal.direction(), proximal.direction());
+      const beta = calculateAngle(proximal.direction(), medial.direction());
+      const gamma = calculateAngle(medial.direction(), distal.direction());
+      return { type, alpha, beta, gamma };
+    });
+  printFingersAngles(samples);
+  return samples
+}
+
+const controller = new Controller(controllerOptions)
+  .on('connect', () => { setInterval(sampleAngles, 500); })
+  .connect();
